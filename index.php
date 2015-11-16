5a1566bdc2f56a96c285c5eb2a1c078769bc3851
Indro <indro.malleus@khk.ee>
2015-11-16 14:05:08 +0200

    index.php ja väike kujundus

---
+++ index.php
@@ -0,0 +1,37 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>watches.ee</title>
+</head>
+<body>
+<div class="banner">
+    <h1>Watches.ee</h1>
+    <p>Parim online kellapood eestis!</p>
+</div>
+<div class="navbar">
+<ul class="navbar">
+    <li><a href="#">Pealeht</a></li>
+    <li><a href="#">Link2</a></li>
+    <li><a href="#">Link3</a></li>
+    <li><a href="#">Link4</a></li>
+</ul>
+</div>
+<div class="main">
+    <div class="pildike"><img src="kell1.jpg" alt="kell" style="width: 100%;px;"></div>
+    <table border="0" style="width:100%; ;">
+        <tr>
+            <th>Pilt</th>
+            <th>Nimi</th>
+            <th>Hind</th>
+        </tr>
+        <tr>
+            <td>Img.jpg.png</td>
+            <td>Kellanimi</td>
+            <td>944 eurot</td>
+        </tr>
+    </table>
+</div>
+
+</body>
+</html>
