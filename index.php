aecb022f923362bafae7321549c12f032024b319
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-18 10:09:54 +0200

    Ühe toote vaade [Delivers #108209772]

--- index.php
+++ index.php
@@ -1,34 +1,48 @@
-<html lang="en">
+<html lang="et">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+    <script type="text/javascript" src="js/lisa_info.js"></script>
     <title>watches.ee</title>
 </head>
 <body>
 <div class="banner">
     <h1>Watches.ee</h1>
+
     <p>Parim online kellapood eestis!</p>
 </div>
 <div class="navbar">
-<ul class="navbar">
-    <li><a href="#">Pealeht</a></li>
-    <li><a href="#">Link2</a></li>
-    <li><a href="#">Link3</a></li>
-    <li><a href="#">Link4</a></li>
-</ul>
+    <ul class="navbar">
+        <li><a href="#">Pealeht</a></li>
+        <li><a href="#">Link2</a></li>
+        <li><a href="#">Link3</a></li>
+        <li><a href="#">Link4</a></li>
+    </ul>
 </div>
 <div class="main">
-    <div class="pildike"><img src="kell1.jpg" alt="kell" style="width: 100%;px;"></div>
+    <div class="pildike"><img src="assets/img/kell1.jpg" alt="kell" style="width: 100%;px;"></div>
     <table border="0" style="width:100%; ;">
         <tr>
             <th>Pilt</th>
             <th>Nimi</th>
             <th>Hind</th>
+            <th>Lisainfo</th>
         </tr>
         <tr>
             <td>Img.jpg.png</td>
-            <td>Kellanimi</td>
-            <td>944 eurot</td>
+            <td>Fossil CH2564</td>
+            <td>195€</td>
+            <td>
+                <a href="#" id="trigger">Loe rohkem.</a>
+                <!-- HIDDEN DIV -->
+                <div id="pop-up">
+                    <h4>Fossil CH2564</h4>
+
+                    Trendikas kronograaf (chronograph) laia nahkrihmaga. Kella korpus on 12 mm paks. Kellarihm on 45 mm
+                    lai.
+                </div>
+            </td>
         </tr>
     </table>
 </div>
