07c7f3c656f8782817a1413601723fa1a72b25a7
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-11-18 11:42:36 +0200

    Master template, views jms. [Delivers #108209772]

---
+++ views/home.php
@@ -0,0 +1,27 @@
+<div class="main">
+    <div class="pildike"><img src="assets/img/kell1.jpg" alt="kell" style="width: 100%;font-family:arial;"></div>
+
+    <table border="0" style="width:100%; ;">
+        <tr>
+            <th>Pilt</th>
+            <th></th>
+            <th>Hind</th>
+            <th>Lisainfo</th>
+        </tr>
+        <tr>
+            <td>Img.jpg.png</td>
+            <td>Fossil CH2564</td>
+            <td>195€</td>
+            <td>
+                <a href="#" id="trigger">Loe rohkem.</a>
+                <!-- HIDDEN DIV -->
+                <div id="pop-up">
+                    <h4>Fossil CH2564</h4>
+
+                    Trendikas kronograaf (chronograph) laia nahkrihmaga. Kella korpus on 12 mm paks. Kellarihm on 45 mm
+                    lai.
+                </div>
+            </td>
+        </tr>
+    </table>
+</div>
