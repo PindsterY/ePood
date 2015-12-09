01cfb84b53140ae4629c0e1631c519e59acf5472
rasmusreimaa <rasmus.reimaa@khk.ee>
2015-12-09 11:18:24 +0200

    config

--- index.php
+++ index.php
@@ -8,3 +8,22 @@ $page = !empty($_GET['page']) ? $_GET['page'] : 'home';
 //Include template
 require "templates/master_template.php";
 
+/*
+$currency = '€ '; //Currency Character or code
+
+$db_username = 'root';
+$db_password = '';
+$db_name = 'kaekellad';
+$db_host = 'localhost';
+
+$shipping_cost      = 1.50; //shipping cost
+$taxes              = array( //List your Taxes percent here.
+    'Käibemaks' => 12,
+    'Teenuse hind' => 5
+);
+// Connect to database.
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+*/
