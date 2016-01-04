9ac16a1a439f02416f493c5de97391a7cf2a4231
Rasmus Pind <rasmus.pind@khk.ee>
2016-01-04 11:22:53 +0200

    Lisasin andmebaasi stuffi!

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$currency = '€ '; //Currency Character or code
+
+define('DATABASE_HOSTNAME', '127.0.0.1');
+define('DATABASE_USERNAME', 'root');
+define('DATABASE_PASSWORD', '');
+define('DATABASE_DATABASE', 'kaekellad');
+
+$shipping_cost      = 1.50; //shipping cost
+$taxes              = array( //List your Taxes percent here.
+'Käibemaks' => 12,
+'Teenuse hind' => 5
+);
